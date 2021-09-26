54b26aea9d87f2614346143daaf7e1790d16bb6c
Mehrsan Askarifirouzjaee <mehrsan@Mehrsans-MacBook-Pro.local>
2021-09-26 00:29:57 -0400

    modifying hello.py for activity 3

--- hello.py
+++ hello.py
@@ -1,13 +1,16 @@
-from flask import Flask
+from flask import Flask, render_template
+from flask_bootstrap import Bootstrap
+from flask_moment import Moment
+from datetime import datetime
+
 app = Flask(__name__)
+bootstrap = Bootstrap(app)
+moment = Moment(app)
 
 @app.route('/')
-def index():
-  return '<h1>Hello World!</h1>'
 
-@app.route('/user/<name>')
-def user(name):
-  return '<h1>Hello, %s!</h1>' % name
+def user():
+  return render_template('user.html', name='Mehrsan', current_time = datetime.utcnow())
 
 if __name__ == '__main__':
   app.run(debug=True)
