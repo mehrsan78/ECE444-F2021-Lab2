1ee038a4fb7b83a45521194757347bbd55ac9897
Mehrsan Askarifirouzjaee <mehrsan@Mehrsans-MacBook-Pro.local>
2021-09-26 01:39:32 -0400

    Activity 4 step2, add email field

--- hello.py
+++ hello.py
@@ -1,46 +1,57 @@
 from flask import Flask, render_template, session, redirect, url_for, flash
 from flask_bootstrap import Bootstrap
-from flask_moment import Moment
 from flask_wtf import Form
 from wtforms import StringField, SubmitField
-from wtforms.validators import Required
-from datetime import datetime
+from wtforms.validators import Required, ValidationError
+
+
 app = Flask(__name__)
 bootstrap = Bootstrap(app)
-moment = Moment(app)
+
 
 app.config['SECRET_KEY'] = 'key'
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    name = None
-    form = NameForm()
+    form = FormUofT()
 
     if form.validate_on_submit():
         old_name = session.get('name')
-        print(old_name)
-        print(form.name.data)
+        old_email = session.get('email')
 
         if old_name is not None and old_name != form.name.data:
             flash('Looks like you have changed your name!')
+        if old_email is not None and old_email != form.email.data:
+            flash('Looks like you have changed your email!')
 
         session['name'] = form.name.data
-        form.name.data = ''
+        session['email'] = form.email.data
         return redirect(url_for('index'))
 
     return render_template(
         'index.html',
-        current_time=datetime.utcnow(),
         form=form,
-        name=session.get('name')
+        name=session.get('name'),
+        email=session.get('email')
     )
+
 @app.route('/user/<name>')
 def user(name):
     return render_template('user.html', name=name)
 
-class NameForm(Form):
+
+class EmailCheck(object):
+
+    def __call__(self, form, field):
+        if '@' not in field.data:
+            message = 'Please include an \'@\' in the email address. \'' + field.data + '\' is missing an \'@\'.'
+            raise ValidationError(message)
+
+class FormUofT(Form):
     name = StringField('What is your name?', validators=[Required()])
+    email = StringField('What is your UofT Email address?', validators=[EmailCheck()])
     submit = SubmitField('Submit')
-    
+
+
 if __name__ == '__main__':
     app.run(debug=True)
